39c8dfba56ee06720b00e297ef64e9d5b3cf8479
Key Yu Wan <keyflcbyuwan@gmail.com>
2023-05-18 10:48:47 -0300

    feat: authenticate controller & e2e

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
 import fastify from 'fastify'
+import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
+
 import { ZodError } from 'zod'
 
 import { env } from './env'
@@ -7,6 +10,19 @@ import { orgsRoutes } from './http/controllers/orgs/routes'
 import { petsRoutes } from './http/controllers/pets/routes'
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  sign: {
+    expiresIn: '10m',
+  },
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+})
+
+app.register(fastifyCookie)
 
 app.register(orgsRoutes)
 app.register(petsRoutes)
